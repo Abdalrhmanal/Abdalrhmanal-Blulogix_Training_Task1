6c6598d803c6ba3cb9ca6272047cc6471b4b6fb1
Abdulrahmanl <ramasy111020033@gmail.com>
2024-04-18 23:01:32 +0300

    End Task 4

--- src/app/task-fourth/task-fourth.component.ts
+++ src/app/task-fourth/task-fourth.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableFourthComponent } from "../../../projects/ui/src/lib/tablt-fourth-third/table.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-task-fourth',
     standalone: true,
     templateUrl: './task-fourth.component.html',
     styleUrl: './task-fourth.component.css',
-    imports: [CommonModule, TableFourthComponent]
+    imports: [CommonModule, TableFourthComponent ,RouterLink]
 })
 export class TaskFourthrComponent {
 
